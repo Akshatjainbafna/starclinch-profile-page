71371ca8afaf1a4f7c26743f5a7daaf0ff8c0fda
Akshat Bafna <86974814+Akshatjainbafna@users.noreply.github.com>
2023-04-09 18:07:20 +0530

    browserrouter-to-hashrouter

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import './App.css';
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import ProfilePage from './pages/profile';
 import FormPage from './pages/form';
 
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route exact path="/RanbirKapoor" Component={ProfilePage} />
         <Route exact path="/cart/add/:celebId" Component={FormPage} />
         <Route path='/' Component={() => {<p>Hello World</p>}} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
